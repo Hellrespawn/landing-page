871310ac18f08d641f052f05fa85a5a468e7904b
Stef Korporaal <stefkorporaal@gmail.com>
2022-01-27 19:23:08 +0100

    Replace ugly build script with gulp

---
+++ gulpfile.ts
@@ -0,0 +1,64 @@
+import { src, dest, parallel, watch as gulpWatch } from 'gulp';
+import postcss from 'gulp-postcss';
+import htmlmin from 'gulp-htmlmin';
+import ts from 'gulp-typescript';
+import uglify from 'gulp-uglify';
+import browserSyncFactory from 'browser-sync';
+
+const browserSyncInstance = browserSyncFactory.create();
+
+export const build = parallel(staticFiles, css, html, typescript);
+
+export function watch(cb: () => void) {
+  gulpWatch('./static/**/*', { ignoreInitial: false }, staticFiles);
+  gulpWatch('./src/*.{css,html,ts}', { ignoreInitial: false }, css);
+  gulpWatch('./src/*.html', { ignoreInitial: false }, html);
+  gulpWatch('./src/*.ts', { ignoreInitial: false }, typescript);
+  cb();
+}
+
+export const dev = parallel(watch, browserSync);
+
+export default build;
+
+function staticFiles() {
+  return src('./static/**/*').pipe(dest('./build'));
+}
+
+function css() {
+  return src('./src/*.css').pipe(postcss()).pipe(dest('./build/'));
+}
+
+function html() {
+  return src('src/*.html')
+    .pipe(
+      htmlmin({
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+      })
+    )
+    .pipe(dest('./build/'));
+}
+
+function typescript() {
+  const tsProject = ts.createProject('tsconfig.json');
+
+  return tsProject
+    .src()
+    .pipe(tsProject())
+    .pipe(uglify())
+    .pipe(dest('./build/'));
+}
+
+function browserSync(cb: () => void) {
+  browserSyncInstance.init({
+    files: ['./static/**/*', './build/**/*'],
+    server: {
+      baseDir: './build',
+    },
+    ui: false,
+    notify: false,
+  });
+  cb();
+}
